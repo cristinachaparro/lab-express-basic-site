6b963bc5e2d921c2fc638a97147c9dbf8a39a983
Alvaro <alvarorivgar@gmail.com>
2023-02-07 17:09:53 +0100

    done, style pending

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
